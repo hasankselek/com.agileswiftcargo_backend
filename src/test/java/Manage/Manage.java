4b6475bf887f41eef7b7b0f3b7701c04a6cd639b
gulpembe <gulpembe.gemicioglu@hotmail.com>
2024-10-03 17:01:47 +0300

    gg03.10

--- src/test/java/Manage/Manage.java
+++ src/test/java/Manage/Manage.java
@@ -11,6 +11,18 @@ public class Manage {
 
     private String delivery_categories="INSERT INTO u201212290_agilesqa.deliverycategories (title, status, position, created_at, updated_at) VALUES (?,?,?,?,?);";
 
+    private String partners ="SELECT name FROM u201212290_agilesqa.partners ORDER BY created_at ASC LIMIT 1";
+
+    private String salary_generates="UPDATE salary_generates SET amount=amount + 10000 WHERE month='2023-09' AND amount=0";
+
+    private String supports = "UPDATE supports SET priority='highest' WHERE service = 'payment'";
+
+
+   // control queries for US25 and US27
+    private String controlQuery_salary="SELECT amount FROM u201212290_agilesqa.salary_generates WHERE month='2023-09' and user_id=28";
+    private String controlQuery_supports="SELECT priority FROM u201212290_agilesqa.supports WHERE service = 'payment'";
+
+
 
 
 
--- src/test/resources/features/DB/DB_US24.feature
+++ src/test/resources/features/DB/DB_US24.feature
@@ -1,8 +1,11 @@
-Feature:
+Feature: Verify the name of our oldest partner in the partners table in the database.
 
   Background: Common Steps
 
     * Database connection established
 
-
-  Scenario:
+  @US24
+  Scenario: DB_TC2401 Verify the name of our oldest partner in the partners table in the database.
+    * Query prepared partners table
+    * Processes the results of the partners table query
+    * Database closed
